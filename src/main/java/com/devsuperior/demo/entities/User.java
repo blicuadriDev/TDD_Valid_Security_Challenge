fa6e3a445377c0ef9652deb8415c996cd0396017
blicuadriDev <blicuadriDev@gmail.com>
2023-09-19 12:19:30 -0300

    User and Role entities + ORM

---
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -0,0 +1,91 @@
+package com.devsuperior.demo.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
+public class User {
+	
+	
+	private Long id;
+	private String email;
+	private String password;
+	
+	
+	@ManyToMany
+	@JoinTable(name="tb_user_role",
+			joinColumns = @JoinColumn(name="user_id"),
+			inverseJoinColumns = @JoinColumn(name="role_id"))
+	private Set<Role> roles = new HashSet<>();
+	
+	
+	public User() {
+	}
+
+
+	public User(Long id, String email, String password) {
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
