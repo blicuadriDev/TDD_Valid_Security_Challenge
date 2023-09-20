cdbedddc2ef8df4d9b92905449ff63628f9934c0
blicuadriDev <blicuadriDev@gmail.com>
2023-09-20 12:54:41 -0300

    Spring checklist OAuth2

--- src/main/java/com/devsuperior/demo/config/AppConfig.java
+++ src/main/java/com/devsuperior/demo/config/AppConfig.java
@@ -2,17 +2,15 @@ package com.devsuperior.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
 	
-		@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable());
-		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		return http.build();
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder ();
 	}
 
 
