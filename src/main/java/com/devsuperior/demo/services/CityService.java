71f250d070e8498a8479e86d6c7fdb3643b8da07
blicuadriDev <blicuadriDev@gmail.com>
2023-09-20 11:03:29 -0300

    Service exceptions; Event Controller + Service layers

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
@@ -16,13 +17,13 @@ public class CityService {
 	@Autowired
 	private CityRepository repository;
 	
-	
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAllCities(){
 		List<City> result = repository.findAll(Sort.by("name"));
 		return result.stream().map(x -> new CityDTO(x)).toList();
 	}
 	
-	
+	@Transactional
 	public CityDTO newCity(CityDTO dto) {
 		City city = new City();
 		city.setId(dto.getId());
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,16 +1,41 @@
 package com.devsuperior.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
 
 @Service
 public class EventService {
 	
 	@Autowired
-	private CityRepository repository;
+	private EventRepository repository;
 	
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllEvtPaged(Pageable pageable){
+		Page<Event> page = repository.findAll(pageable);
+		return page.map(x -> new EventDTO(x));
+	}
+	
+	
+	@Transactional
+	public EventDTO insertEvt(EventDTO dto) {
+		Event entity = new Event();
+		entity.setDate(dto.getDate());
+		entity.setName(dto.getName());
+		entity.setUrl(dto.getUrl());
+		entity.setCity(new City(dto.getCityId(), null));
+		
+		entity = repository.save(entity);
+		
+		return new EventDTO(entity);
+	}
 
 
 }
