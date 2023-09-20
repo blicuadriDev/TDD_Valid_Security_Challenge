a93a72bbe6c89727bfa3cbbb5f6b9fd672b1241e
blicuadriDev <blicuadriDev@gmail.com>
2023-09-20 14:28:29 -0300

    Tests implemented successfully. I guess. I hope.

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value ="/cities")
@@ -29,8 +32,9 @@ public class CityController {
 		return ResponseEntity.ok(list);
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
-	public ResponseEntity<CityDTO> city(@RequestBody CityDTO dto){
+	public ResponseEntity<CityDTO> city(@Valid @RequestBody CityDTO dto){
 		CityDTO cityDto = service.newCity(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
